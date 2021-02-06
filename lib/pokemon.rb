ae364d547deb87eb12f9b67af940b3b633a21667
Vanessa Fiechtner <fiechtnervanessa@gmail.com>
2021-02-06 18:29:24 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,13 @@
 class Pokemon
+  attr_accessor :id, :name, :type, :db
+
+  def initialize(keywords)
+
+  end
+
+  def self.save(name, type, db)
+    db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)", name, type)
+  end
+  
+
 end
